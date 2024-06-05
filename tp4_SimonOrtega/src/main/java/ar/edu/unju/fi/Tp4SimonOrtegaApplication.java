e57e5ca632301ffeec8e7d6686bec800312a34ed
Simón Ortega <Simn Ortega@DESKTOP-D90QPBM>
2024-06-05 13:38:57 -0300

    Inicializacion de la estructura del proyecto

---
+++ tp4_SimonOrtega/src/main/java/ar/edu/unju/fi/Tp4SimonOrtegaApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp4SimonOrtegaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp4SimonOrtegaApplication.class, args);
+	}
+
+}
